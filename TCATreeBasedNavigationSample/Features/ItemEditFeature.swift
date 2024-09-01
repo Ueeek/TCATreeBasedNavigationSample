0d8f5ac386bc03f576e1b61e0e8d59d476060518
Ueeek <uekikoichiro@gmail.com>
2024-09-01 18:09:08 +0900

    add itemAdd, item Edit

---
+++ TCATreeBasedNavigationSample/Features/ItemEditFeature.swift
@@ -0,0 +1,34 @@
+//
+//  ItemEditFeature.swift
+//  TCATreeBasedNavigationSample
+//
+//  Created by KoichiroUeki on 2024/09/01.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+@Reducer
+struct ItemEditFeature {
+    @ObservableState
+    struct State: Equatable {
+        var item: String
+    }
+    
+    enum Action: Sendable {}
+    
+    var body: some Reducer<State, Action> {
+        Reduce { _, _ in
+            return .none
+        }
+    }
+}
+
+struct ItemEditFeatureView: View {
+    @Bindable var store: StoreOf<ItemDetailFeature>
+    
+    var body: some View {
+        Text(" Item Edit of \(store.item)")
+            .navigationTitle("Item Edit View")
+    }
+}
